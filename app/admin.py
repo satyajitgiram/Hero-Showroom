9334359da45793d92a0f3026d3f41e332eae6503
satyajitgiram <satyajitgiram2000@gmail.com>
2023-07-16 19:34:14 +0530

    removed the error from admin.py file

--- app/admin.py
+++ app/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import TestRide, Bike, Color, ContactUs, Category, Gallery
-=======
-from .models import TestRide, Bike, Color, ContactUs, Category
->>>>>>> e3955f889c4891f4cc46ce9a4eb47279c497a143
 # Register your models here.
 
 admin.site.register(TestRide)
 admin.site.register(Bike)
 admin.site.register(Color)
 admin.site.register(ContactUs)
-<<<<<<< HEAD
 admin.site.register(Category)
 admin.site.register(Gallery)
-=======
-admin.site.register(Category)
->>>>>>> e3955f889c4891f4cc46ce9a4eb47279c497a143

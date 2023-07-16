f68afbe36f393e42bcc6159a1fd2fe38760f8d24
satyajitgiram <satyajitgiram2000@gmail.com>
2023-07-16 19:40:34 +0530

    done debug True in settings

--- showroom/settings.py
+++ showroom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)mne!ie75bsk1hrtpao-@4e*-gw=*4b@1!!8-b8v%wpfqodxm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
